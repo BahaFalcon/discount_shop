4e83781ed22e6824a95b9bb1b151cd349615a311
baha <bakyt.malikov53@gmail.com>
2023-03-30 14:31:22 +0600

    second commit

--- shop_api/settings.py
+++ shop_api/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-ck3ud#7*x(6f2v2+t$&l-st)gds3ehn$_%sdu1+2fxpj0$a_a+'
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
